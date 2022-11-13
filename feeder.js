49dca6e3a80479e0a4a8100148bda548450c32e2
BearTheCoder <bearthecoder@gmail.com>
2022-11-13 10:30:12 -0500

    cringe

--- feeder.js
+++ feeder.js
@@ -1,4 +1,4 @@
 const express = require("express");
 const app = express();
-app.listen('0.0.0.0:$bingofeeder-production.up.railway.app', () => console.log("listening..."));
+app.listen('$PORT', () => console.log("listening..."));
 app.use(express.static('public'));
