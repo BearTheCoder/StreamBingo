b5fd2029fa844d28970a0a7f8083c623c0578b1a
BearTheCoder <bearthecoder@gmail.com>
2022-11-13 10:13:06 -0500

    0.0.0.0:$PORT?

--- feeder.js
+++ feeder.js
@@ -1,4 +1,4 @@
 const express = require("express");
 const app = express();
-app.listen('$PORT', () => console.log("listening..."));
+app.listen('0.0.0.0:$PORT', () => console.log("listening..."));
 app.use(express.static('public'));
