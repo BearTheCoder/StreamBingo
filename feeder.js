cedf1825980ba52f7e459b0c02cc80e7384e5947
BearTheCoder <bearthecoder@gmail.com>
2022-11-13 11:17:37 -0500

    I'm hungry

--- feeder.js
+++ feeder.js
@@ -3,9 +3,9 @@ require('dotenv').config;
 const app = express();
 app.listen(process.env.PORT, () => console.log("listening..."));
 app.use(express.static('public'));
-
-
+app.use(express.json())
 
 app.post('/api', (request, response) => {
-  console.log(request.body.name);
+  console.log(request.body);
+  response.end();
 })
