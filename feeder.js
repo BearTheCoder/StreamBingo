8b6a3707a49080439ed1713b0269c5e4cf686a2f
BearTheCoder <bearthecoder@gmail.com>
2022-11-13 10:35:06 -0500

    sadsfsadgass

--- feeder.js
+++ feeder.js
@@ -1,5 +1,5 @@
 const express = require("express");
 require('dotenv').config;
 const app = express();
-app.listen(`0.0.0.0:${process.env.$PORT}`, () => console.log("listening..."));
+app.listen(`0.0.0.0:${process.env.PORT}`, () => console.log("listening..."));
 app.use(express.static('public'));
