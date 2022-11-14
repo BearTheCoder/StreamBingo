c5f8c62f34464def647c55d44f12bbbdc0ee3c66
BearTheCoder <bearthecoder@gmail.com>
2022-11-14 14:24:38 -0500

    testing

--- feeder.js
+++ feeder.js
@@ -1,7 +1,7 @@
-require('dotenv').config;
+require('dotenv').config; //process.env.VARIABLE used for Railway deployment
 const express = require("express");
 const app = express();
-app.listen(process.env.PORT, () => console.log("listening...")); // Railway Deploy
+app.listen(process.env.PORT, () => console.log("listening..."));
 app.use(express.static('public'));
 app.use(express.json());
 
@@ -45,7 +45,7 @@ function getCategories(feederObject, headers) {
       for (let category of dataObject.data) {
         feederObject.categories.push(category.name);
       }
-      if (categories.length !== 0) {
+      if (feederObjectcategories.length !== 0) {
         getStreams(feederObject, headers);
         responseTimout(feederObject, "success");
       }
