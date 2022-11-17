700d967b3b5b0f80f4cc8d9f3b16f52aca19b7c4
BearTheCoder <bearthecoder@gmail.com>
2022-11-17 17:33:30 -0500

    Lower timer to debug

--- feeder.js
+++ feeder.js
@@ -11,16 +11,16 @@ let streamArray = [];
 let newStreamArray = [];
 
 getAuthHeader();
-setInterval(getAuthHeader, 1800000); //Move Get Auth here 30-min timeout?
+setInterval(getAuthHeader, 500000); //Move Get Auth here 30-min timeout?
 
-function getAuthHeader () {
-  const url = `https://id.twitch.tv/oauth2/token?client_id=${ process.env.ClientID }&client_secret=${ process.env.ClientSecret }&grant_type=client_credentials`;
+function getAuthHeader() {
+  const url = `https://id.twitch.tv/oauth2/token?client_id=${process.env.ClientID}&client_secret=${process.env.ClientSecret}&grant_type=client_credentials`;
   fetch(url, { method: "POST" })
     .then(res => res.json())
     .then(authorizationObject => {
       const upperCaseSubstring = authorizationObject.token_type.substring(0, 1).toUpperCase();
       const tokenSubstring = authorizationObject.token_type.substring(1, authorizationObject.token_type.length);
-      const authorization = `${ upperCaseSubstring + tokenSubstring } ${ authorizationObject.access_token }`;
+      const authorization = `${upperCaseSubstring + tokenSubstring} ${authorizationObject.access_token}`;
       const headers = { authorization, "Client-Id": process.env.ClientID, };
       getStreams(headers, "");
     });
@@ -29,11 +29,11 @@ function getAuthHeader () {
   }, 90000);
 }
 
-function getStreams (headers, pageNo) {
+function getStreams(headers, pageNo) {
   if (pageNo !== undefined && stopLoading === false) {
     let streamsEndpoint = "";
     if (pageNo === "") streamsEndpoint = `https://api.twitch.tv/helix/streams?language=en&first=100`;
-    else streamsEndpoint = `https://api.twitch.tv/helix/streams?language=en&first=100&after=${ pageNo }`;
+    else streamsEndpoint = `https://api.twitch.tv/helix/streams?language=en&first=100&after=${pageNo}`;
     fetch(streamsEndpoint, { headers })
       .then(res => res.json())
       .then(dataObject => {
@@ -47,7 +47,7 @@ function getStreams (headers, pageNo) {
     streamArray = Array.from(newStreamArray);
     newStreamArray = [];
     const timeoutReason = pageNo === undefined ? 'pageNo undefined' : "timeout";
-    console.log(`Loading stoped. Reason: ${ timeoutReason }`);
+    console.log(`Loading stoped. Reason: ${timeoutReason}`);
   }
 }
 
