9b72ff5e15dc8091755a120f763019b23974aee5
BearTheCoder <bearthecoder@gmail.com>
2022-11-17 12:46:42 -0500

    Trying New Loading

--- feeder.js
+++ feeder.js
@@ -1,94 +1,79 @@
 require('dotenv').config; //process.env.VARIABLE used for Railway deployment
+
 const express = require("express");
 const app = express();
 app.listen(process.env.PORT, () => console.log("listening..."));
 app.use(express.static('public'));
 app.use(express.json());
 
-app.post('/startStreams', (postRequest, postResponse) => {
+let stopLoading = false;
+let streamArray = [];
 
-  let feederObject = {
-    request: postRequest,
-    response: postResponse,
-    streamArray: [],
-    categories: [],
-    stopLoading: false,
-    responseTime: 90000,
-    pageNo: "",
-  };
+getAuthHeader();
+setInterval(getAuthHeader, 1800000); //Move Get Auth here 30-min timeout?
 
-  const url = `https://id.twitch.tv/oauth2/token?client_id=${process.env.ClientID}&client_secret=${process.env.ClientSecret}&grant_type=client_credentials`;
+function getAuthHeader () {
+  const url = `https://id.twitch.tv/oauth2/token?client_id=${ process.env.ClientID }&client_secret=${ process.env.ClientSecret }&grant_type=client_credentials`;
   fetch(url, { method: "POST" })
     .then(res => res.json())
     .then(authorizationObject => {
       const upperCaseSubstring = authorizationObject.token_type.substring(0, 1).toUpperCase();
       const tokenSubstring = authorizationObject.token_type.substring(1, authorizationObject.token_type.length);
-      const authorization = `${upperCaseSubstring + tokenSubstring} ${authorizationObject.access_token}`;
+      const authorization = `${ upperCaseSubstring + tokenSubstring } ${ authorizationObject.access_token }`;
       const headers = { authorization, "Client-Id": process.env.ClientID, };
-
-      if (feederObject.request.body.searchQuery !== "") {
-        getCategories(feederObject, headers);
-      }
-      else {
-        getStreams(feederObject, headers);
-        responseTimout(feederObject, "success");
-      }
+      getStreams(headers, "");
     });
-});
-
-function getCategories(feederObject, headers) {
-  feederObject.categories = [];
-  let categoryEndpoint = `https://api.twitch.tv/helix/search/categories?query=${feederObject.request.body.searchQuery}`;
-  fetch(categoryEndpoint, { headers })
-    .then(res => res.json())
-    .then(dataObject => {
-      for (let category of dataObject.data) {
-        feederObject.categories.push(category.name);
-      }
-      if (feederObject.categories.length !== 0) {
-        getStreams(feederObject, headers);
-        responseTimout(feederObject, "success");
-      }
-      else {
-        feederObject.stopLoading = true;
-        feederObject.responseTime = 1000;
-        responseTimout(feederObject, "failure");
-      }
-    });
+  setTimeout(() => {
+    stopLoading = true;
+    console.log('Loading finished...');
+  }, 90000);
 }
 
-function getStreams(feederObject, headers) {
-  if (feederObject.pageNo !== undefined && !feederObject.stopLoading) {
+function getStreams (headers, pageNo) {
+  newStreamArray = [];
+  if (pageNo !== undefined || stopLoading === true) {
     let streamsEndpoint = "";
-    if (feederObject.pageNo === "") streamsEndpoint = `https://api.twitch.tv/helix/streams?language=en&first=100`;
-    else streamsEndpoint = `https://api.twitch.tv/helix/streams?language=en&first=100&after=${feederObject.pageNo}`;
+    if (pageNo === "") streamsEndpoint = `https://api.twitch.tv/helix/streams?language=en&first=100`;
+    else streamsEndpoint = `https://api.twitch.tv/helix/streams?language=en&first=100&after=${ pageNo }`;
     fetch(streamsEndpoint, { headers })
       .then(res => res.json())
       .then(dataObject => {
         for (let user of dataObject.data) {
-          if (feederObject.categories.length !== 0) {
-            if (user.viewer_count <= feederObject.request.body.maxViewers && feederObject.categories.includes(user.game_name)) {
-              feederObject.streamArray.push(user);
-            }
-          }
-          else if (user.viewer_count <= feederObject.request.body.maxViewers) {
-            feederObject.streamArray.push(user);
-          }
+          newStreamArray.push(user);
         }
-        feederObject.pageNo = dataObject.pagination.cursor;
-        getStreams(feederObject, headers);
+        getStreams(headers, dataObject.pagination.cursor);
       });
+  }
+  else {
+    streamArray = Array.from(newStreamArray);
+    newStreamArray = [];
   }
 }
 
-function responseTimout(feederObject, status) {
-  setTimeout(() => {
-    feederObject.stopLoading = true;
-    if (status === "success") console.log(`Loading Finalized. Total Streams: ${feederObject.streamArray.length}`);
-    else if (status === "failure") console.log(`Load ended in failure, sent alert to client...`);
-    feederObject.response.json({
-      status: status,
-      streams: feederObject.streamArray
-    });
-  }, feederObject.responseTime);
-}
+app.post('/streams', (request, response) => {
+  response.json({
+    streams: streamArray,
+  });
+});
+
+// function getCategories (feederObject, headers) {
+//   feederObject.categories = [];
+//   let categoryEndpoint = `https://api.twitch.tv/helix/search/categories?query=${ feederObject.request.body.searchQuery }`;
+//   fetch(categoryEndpoint, { headers })
+//     .then(res => res.json())
+//     .then(dataObject => {
+//       for (let category of dataObject.data) {
+//         feederObject.categories.push(category.name);
+//       }
+//       if (feederObject.categories.length !== 0) {
+//         getStreams(feederObject, headers);
+//         loadTimeout(feederObject, "success");
+//       }
+//       else {
+//         feederObject.stopLoading = true;
+//         feederObject.responseTime = 1000;
+//         loadTimeout(feederObject, "failure");
+//       }
+//     });
+// }
+
--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+const { json } = require("express");
+
 const nextStreamButton = document.getElementById('nextStreamButton');
 const resetStreamsButton = document.getElementById('resetStreamsButton');
 const startCardButton = document.getElementById('startCardButton');
@@ -11,68 +13,75 @@ resetCardButton.disabled = true;
 
 let streamArray = [];
 
-function loadStreams() {
-  const maxViewers = document.getElementById("MaxViewCount").value === "" ? 1000000 : parseInt(document.getElementById('MaxViewCount').value);
-  maxViewers >= 1 ? maxViewers : 1;
-  const searchQuery = document.getElementById("CategoryInput").value;
+function loadStreams () {
   streamArray = [];
-  const data = {
-    maxViewers: maxViewers,
-    searchQuery: searchQuery,
-  };
   const options = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(data),
   };
-  fetch('/startStreams', options) //post
+  fetch('/streams', options) //post
     .then(promise => promise.json())
     .then(jsonResponse => {
-      if (jsonResponse.status === "success") {
-        const gameName = jsonResponse.streams[0].game_name;
-        const viewerCount = jsonResponse.streams[0].viewer_count;
-        const userName = jsonResponse.streams[0].user_name;
-        const loadedInfoNode = document.getElementById("LoadedUser");
-        loadedInfoNode.innerHTML = `${userName} is streaming ${gameName} to ${viewerCount} viewers.`;
-        document.getElementById("twitch-embed").innerHTML = '';
-        new Twitch.Player(document.getElementById("twitch-embed"), { channel: userName });
-        document.getElementById('PageHeader').innerHTML = `Streams Loaded: ${jsonResponse.streams.length}`;
-        streamArray = jsonResponse.streams;
-        nextStreamButton.disabled = false;
-      }
-      else if (jsonResponse.status === "failure") {
-        alert("Search query returned no results, please try a different term.");
-        resetStreamHTML();
-      }
+
+      filterStreams(jsonResponse.streams);
     });
-  resetStreamsButton.disabled = false;
-  let timeToLoad = 90;
-  const timer = document.getElementById("twitch-embed");
-  timer.innerHTML = `<img src="loading-gif.gif" width="100"/><h4 id="tempTimer">Loading Streams 90 seconds left...</h4>`;
-  let tempTimer = document.getElementById('tempTimer');
-  setInterval(() => {
-    tempTimer.innerText = `Loading Streams ${timeToLoad--} seconds left...`;
-  }, 1000);
 };
 
-function loadNextStream() {
-  let RandNum = Math.floor(Math.random() * streamArray.length);
-  document.getElementById("LoadedUser").innerHTML =
-    `${streamArray[RandNum].user_name} is streaming ${streamArray[RandNum].game_name} 
-    to ${streamArray[RandNum].viewer_count} viewers.`;
-  document.getElementById("twitch-embed").innerHTML = '';
-  new Twitch.Player(document.getElementById("twitch-embed"), { channel: streamArray[RandNum].user_login });
+function filterStreams (streams) {
+  const maxViewers = document.getElementById("MaxViewCount").value === "" ? 1000000 : parseInt(document.getElementById('MaxViewCount').value);
+  const searchQuery = document.getElementById("CategoryInput").value;
+  maxViewers >= 1 ? maxViewers : 1;
+
+  for (let user of streams) {
+    if (user.viewer_count <= feederObject.request.body.maxViewers) {
+      if (searchQuery === "") {
+        streamArray.push(user);
+      }
+      else if (user.game_name.includes(searchQuery)) {
+        streamArray.push(user);
+      }
+    }
+  }
+
+  if (streamArray.length === 0) {
+    alert("Search query returned no results. Try a different search query.");
+    resetStreamHTML();
+  }
+  else { loadFirstStream(); }
 }
 
-function resetStreamHTML() {
+function loadFirstStream () {
+  let RandNum = Math.floor(Math.random() * streamArray.length);
+  const gameName = jsonResponse.streams[ RandNum ].game_name;
+  const viewerCount = jsonResponse.streams[ RandNum ].viewer_count;
+  const userName = jsonResponse.streams[ RandNum ].user_name;
+  const loadedInfoNode = document.getElementById("LoadedUser");
+  loadedInfoNode.innerHTML = `${ userName } is streaming ${ gameName } to ${ viewerCount } viewers.`;
+  document.getElementById("twitch-embed").innerHTML = '';
+  new Twitch.Player(document.getElementById("twitch-embed"), { channel: userName });
+  document.getElementById('PageHeader').innerHTML = `Streams Loaded: ${ jsonResponse.streams.length }`;
+  nextStreamButton.disabled = false;
+  resetStreamsButton.disabled = false;
+}
+
+function loadNextStream () {
+  let RandNum = Math.floor(Math.random() * streamArray.length);
+  document.getElementById("LoadedUser").innerHTML =
+    `${ streamArray[ RandNum ].user_name } is streaming ${ streamArray[ RandNum ].game_name } 
+    to ${ streamArray[ RandNum ].viewer_count } viewers.`;
+  document.getElementById("twitch-embed").innerHTML = '';
+  new Twitch.Player(document.getElementById("twitch-embed"), { channel: streamArray[ RandNum ].user_login });
+}
+
+function resetStreamHTML () {
   document.getElementById("twitch-embed").innerHTML = defaultScreen;
   document.getElementById("LoadedUser").innerHTML = 'Load streams to get stream information.';
   document.getElementById("PageHeader").innerHTML = `Streams Loaded: 0`;
 }
 
-function generateRandomBingoCard() {
+function generateRandomBingoCard () {
   let bingoSquares = document.querySelectorAll("textarea");
   let duplicateArray = [];
   BingoCardDefaultOptions.forEach((option) => {
@@ -80,12 +89,12 @@ function generateRandomBingoCard() {
   });
   bingoSquares.forEach((node) => {
     let randomNumber = Math.floor(Math.random() * duplicateArray.length);
-    node.value = duplicateArray[randomNumber];
+    node.value = duplicateArray[ randomNumber ];
     duplicateArray.splice(randomNumber, 1);
   });
 }
 
-function startBingoCard() {
+function startBingoCard () {
   const bingoSquares = document.querySelectorAll("textarea");
   const canStart = true;
   for (let node of bingoSquares) {
@@ -96,7 +105,7 @@ function startBingoCard() {
   }
   if (canStart) {
     for (let node of bingoSquares) {
-      node.outerHTML = `<p>${node.value}</p>`;
+      node.outerHTML = `<p>${ node.value }</p>`;
     }
     const pArray = document.querySelectorAll("p");
     pArray.forEach((node) => {
@@ -113,7 +122,7 @@ function startBingoCard() {
   }
 }
 
-function resetBingoCard() {
+function resetBingoCard () {
   const pArray = document.querySelectorAll("p");
   for (let node of pArray) {
     node.outerHTML = '<textarea placeholder="Enter A Square" cols="13" rows="4" maxlength="55"></textarea>';
