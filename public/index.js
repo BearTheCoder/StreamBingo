05e649aea11a3c6b9fe76e29fb2c29b1eaf73411
Bear <bearthecoder@gmail.com>
2023-05-11 21:51:18 -0400

    duh

--- public/index.js
+++ public/index.js
@@ -22,7 +22,7 @@ function getSearchQuery() {
   const maxViewers = document.getElementById("MaxViewCount").value === "" ? 10000000 : parseInt(document.getElementById('MaxViewCount').value); // Handles no input
   maxViewers >= 1 ? maxViewers : 1; // Handles negative input
   const searchQuery = document.getElementById("CategoryInput").value;
-  const loadTime = parseInt(document.getElementById("loadTime"));
+  const loadTime = parseInt(document.getElementById("loadTime").value);
   return {
     method: 'POST',
     headers: {
