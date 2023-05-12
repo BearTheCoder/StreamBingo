3bb5d424ec6bc4d0609281127bda9b60cbc6180f
Bear <bearthecoder@gmail.com>
2023-05-11 20:47:20 -0400

    forgot a dollar sign

--- public/index.js
+++ public/index.js
@@ -81,7 +81,7 @@ function loadNextStream() {
 
 function loadChat(channel) {
   const chatEmbed = document.getElementById("chat_embed");
-  chatEmbed.src = `https://www.twitch.tv/embed/{channel}/chat?parent=streambingo.live/`
+  chatEmbed.src = `https://www.twitch.tv/embed/${channel}/chat?parent=streambingo.live/`
 }
 
 function resetStreamHTML() {
